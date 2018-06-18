ca5b18b97e3f709753c3f66f448625930e306b40
JunoJulius <yunoyulius@gmail.com>
2018-06-18 19:02:39 +0200

    pasted and edited old logic, created landingContainer for Login/Register + added Route to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom'
+import LandingContainer from './components/LandingContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Route exact path="/" component={LandingContainer} />
+
 
       </div>
     );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      LoginMail: '',
+      LoginPassword: ''
+    }
+  }
+  onEmailChange = (event) => {
+    this.setState({LoginMail: event.target.value})
+  }
+  onPasswordChange = (event) => {
+    this.setState({LoginPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    // fetch('', {
+    //   method: 'post',
+    //   headers: {'Content-Type': 'application/json'},
+    //   body: JSON.stringify({
+    //   email: this.state.LoginMail,
+    //   password: this.state.LoginPassword
+    //   })
+    // })
+    .then(response => response.json())
+    .then(user => {
+      if (user.id){
+        this.props.loadUser(user);
+        this.props.onRouteChange('home');
+
+      }
+    })
+  }
+  render() {
+    const { onRouteChange } = this.props;
+    return(
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+
+            </fieldset>
+
+        </main>
+        </article>
+    );
+  }
+}
+
+export default Login;
