f0c03d367a3620a2a7abf9e951f46d14620acd0c
JunoJulius <yunoyulius@gmail.com>
2018-06-19 07:20:14 +0200

    edited login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-
-
 class Login extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       LoginMail: '',
@@ -11,28 +9,36 @@ class Login extends React.Component {
     }
   }
   onEmailChange = (event) => {
-    this.setState({LoginMail: event.target.value})
+    // this.setState({LoginMail: event.target.value})
   }
   onPasswordChange = (event) => {
-    this.setState({LoginPassword: event.target.value})
+    // this.setState({LoginPassword: event.target.value})
   }
 
   onSubmitLogin = () => {
-// Handle Login 
+    // Handle Login
+    // this.state.LoginMail
+    // this.state.LoginPassword
   }
   render() {
-    const { onRouteChange } = this.props;
-    return(
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+    // const { onRouteChange } = this.props;
+    return (<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">
 
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+          <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+          <div className="mt3">
+            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address"/>
+          </div>
+          <div className="mv3">
+            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password"/>
+          </div>
+        </fieldset>
 
-            </fieldset>
-
-        </main>
-        </article>
-    );
+      </main>
+    </article>);
   }
 }
 
