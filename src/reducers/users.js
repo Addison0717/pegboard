4a7f9ec213b02cfb9b7a7a4080004cd2e7898c52
JunoJulius <yunoyulius@gmail.com>
2018-06-20 12:17:28 +0200

    user-reducer added

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -66,33 +66,27 @@ const users = {
 
 
 
-export default (state = users, action = {}) => {
+        export default (state = users, action = {}) => {
 
-  switch (action.type) {
-  case 'SELECT_USER':
-    return action.payload
-  case 'NEW_USER':
-    return {...state, [action.payload.Id]: action.payload.body}
-  case 'MAKE_REQUEST':
-    return JSON.parse(JSON.stringify(state))[action.payload.posterId].notification.request.push(action.payload)
-  // case 'MAKE_MATCH':
-  //   return JSON.parse(JSON.stringify(state)).map((user)=>{
-  //     if (user.userId=== action.payload.posterId) {
-  //           user.notification.request = user.notification.request.filter((reqItem)=>{
-  //             return reqItem.postBody.postId !== action.payload.postBody.postId
-  //           })
-  //
-  //
-  //           user.notification.match.push( action.payload)
-  //
-  //           return user
-  //
-  //
-  //     }else {
-  //       return user
-  //     }
-  //   })
-  default:
-    return state
-  }
-}
+         switch (action.type) {
+         case ‘NEW_USER’:
+           return [...state, action.payload]
+         case ‘MAKE_REQUEST’:
+           const pushRequest = JSON.parse(JSON.stringify(state))
+           pushRequest[action.payload.posterId].notification.request.push(action.payload)
+           return pushRequest
+         case ‘MAKE_MATCH’:
+           const match = JSON.parse(JSON.stringify(state))
+           match[action.payload.posterId].notification.request =  match[action.payload.posterId].notification.request
+           .filter((reqItem)=>{ return (reqItem.postBody.postId !== action.payload.postBody.postId )})
+
+           match[action.payload.posterId].notification.match.push( action.payload)
+           return match
+         case ‘MAKE_POST’:
+           const Post = JSON.parse(JSON.stringify(state))
+           Post[action.payload.posterId].notification.posts.push(action.payload)
+           return Post
+         default:
+           return state
+         }
+        }
